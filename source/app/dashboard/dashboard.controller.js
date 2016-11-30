d08e73766811a8556e0241ab27330dce6e0ce5f4
Sergiy Kuzmin <erngorn@gmail.com>
2016-11-30 13:17:52 +0200

    Added animation to grid

--- source/app/dashboard/dashboard.controller.js
+++ source/app/dashboard/dashboard.controller.js
@@ -14,16 +14,35 @@
 			vm.testData = data;
 
 			vm.removeWidget = removeWidget;
+			vm.sort = sort;
+            vm.gridStack = {};
+
+			setPosition();
 
 			function removeWidget(w) {
                 var index = vm.testData.indexOf(w);
                 vm.testData.splice(index, 1);
             };
 
+            function sort() {
+            	vm.testData = _.sortBy(vm.testData, 'id');
+            	setPosition();
+            }
+
+            function setPosition() {
+            	_.forEach(vm.testData, function(item, i) {
+            		item.position = i;
+            	});
+            }
+
             vm.options = {
                 cellHeight: 200,
                 verticalMargin: 10
             };
+
+            setTimeout(function(){
+                vm.gridStack.set_animation(true);
+            },0);
 
  		}
 })();
