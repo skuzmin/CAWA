e4e0299445813d37bc453cb5bde3a54877303ac0
skuzmin <erngorn@gmail.com>
2016-12-04 20:41:20 +0200

    added search fields, removed duplicated code from controller

--- source/app/decision/decision.controller.js
+++ source/app/decision/decision.controller.js
@@ -14,20 +14,6 @@
 			vm.testData = data;
 			vm.sort = sort;
 
-$('.divider').draggable({
-			    axis: 'x', 
-			    containment: 'parent',
-			    drag: function (event, ui) { 
-			        var 
-			        	newWidth = ui.offset.left, 
-			        	criteriaWidth = $(this).prev().width(),
-			        	dicisionWidth = $(this).next().width();
-			        	
-			        $(this).next().width(dicisionWidth + (criteriaWidth - newWidth));
-			        $(this).prev().width(newWidth);
-			    } 
-			});
-
             function sort() {
             	DecisionService.getTestData2().then(function(result) {
             		vm.updateList = result;
