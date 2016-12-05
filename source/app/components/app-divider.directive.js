f43e999e598317832cd7f974175a72f4cfcf90f0
skuzmin <erngorn@gmail.com>
2016-12-05 23:13:09 +0200

    refactored code, added height directive

---
+++ source/app/components/app-divider.directive.js
@@ -0,0 +1,50 @@
+/* Required jQuery(full) and jQueryUI */
+(function() {
+
+    'use strict';
+
+    angular
+        .module('app.components')
+        .directive('appDivider', appDivider);
+
+    function appDivider() {
+        var directive = {
+            restrict: 'E',
+            replace: 'true',
+            template: '<div class="divider"></div>',
+            scope: {
+                axis: '@',
+                side: '@'
+            },
+            link: link
+        };
+
+        return directive;
+
+        function link(scope, elem, attrs) {
+            elem.draggable({
+                axis: scope.axis,
+                containment: 'parent',
+                drag: function(event, ui) {
+                    var
+                        leftElementCurrentWidth = elem.prev().width(),
+                        rightElementCurrentWidth = elem.next().width(),
+                        newLeftElementWidth,
+                        newRightElementWidth;
+                    if (scope.side === 'right') {
+                        newRightElementWidth = $(window).width() - (ui.offset.left + elem.outerWidth());
+                        newLeftElementWidth = leftElementCurrentWidth + (rightElementCurrentWidth - newRightElementWidth);
+                    } else {
+                        newLeftElementWidth = ui.offset.left;
+                        newRightElementWidth = rightElementCurrentWidth + (leftElementCurrentWidth - newLeftElementWidth);
+                    }
+                    if (newRightElementWidth >= 150 && newLeftElementWidth >= 150) {
+                        elem.next().width(newRightElementWidth);
+                        elem.prev().width(newLeftElementWidth);
+                    }
+                }
+            });
+        }
+    }
+
+})();
--- source/app/decision/decision.controller.js
+++ source/app/decision/decision.controller.js
@@ -6,31 +6,14 @@
 		.module('app.decision')
 		.controller('DecisionController', DecisionController);
 
-		DecisionController.$inject = ['data', 'DecisionService'];
+		DecisionController.$inject = ['data', 'DecisionService', '$scope'];
 
-		function DecisionController(data, DecisionService) {
+		function DecisionController(data, DecisionService, $scope) {
 			var vm = this;
-
-			vm.testData = data;
+	
 			vm.sort = sort;
-
-			$(window).on('resize', function(){
-				var a = $(window).height() - $('.app-header').outerHeight() - $('.app-footer').outerHeight() - $('.top-panel').outerHeight();
-				var mainH = $('.main-panel').outerHeight();
-
-				if(mainH > a) {
-					a = mainH;
-				}
-				$('.js-full-height').css('min-height', a);
-			 });
-
-			var a = $(window).height() - $('.app-header').height() - $('.app-footer').height() - $('.top-panel').height();
-			var mainH = $('.main-panel').outerHeight();
-
-				if(mainH > a) {
-					a = mainH;
-				}
-			$('.js-full-height').css('min-height', a);
+			vm.testData = data;
+			vm.elements = ['app-header', 'app-footer', 'top-panel'];
 
             function sort() {
             	DecisionService.getTestData2().then(function(result) {
