8f5a0978a125da214343198d5e15550759d47225
Sergiy Kuzmin <erngorn@gmail.com>
2016-12-05 12:38:55 +0200

    refactored deviders fixed ui bugs

--- source/app/decision/decision.controller.js
+++ source/app/decision/decision.controller.js
@@ -14,6 +14,24 @@
 			vm.testData = data;
 			vm.sort = sort;
 
+			$(window).on('resize', function(){
+				var a = $(window).height() - $('.app-header').outerHeight() - $('.app-footer').outerHeight() - $('.top-panel').outerHeight();
+				var mainH = $('.main-panel').outerHeight();
+
+				if(mainH > a) {
+					a = mainH;
+				}
+				$('.js-full-height').css('min-height', a);
+			 });
+
+			var a = $(window).height() - $('.app-header').height() - $('.app-footer').height() - $('.top-panel').height();
+			var mainH = $('.main-panel').outerHeight();
+
+				if(mainH > a) {
+					a = mainH;
+				}
+			$('.js-full-height').css('min-height', a);
+
             function sort() {
             	DecisionService.getTestData2().then(function(result) {
             		vm.updateList = result;
