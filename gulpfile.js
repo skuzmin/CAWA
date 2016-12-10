bcab5e65bd05c603090544f56a589a52adaefa50
skuzmin <erngorn@gmail.com>
2016-12-10 17:50:37 +0200

    added injections

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,20 @@ var gulp = require('gulp'),
     less = require('gulp-less'),
     concatCss = require('gulp-concat-css'),
     plumber = require('gulp-plumber'),
+    autoprefixer = require('gulp-autoprefixer'),
+    util = require('gulp-util'),
+    rename = require('gulp-rename'),
     inject = require('gulp-inject');
 
-//watchers
+// watchers
 gulp.task('watch', function() {
     gulp.watch('source/app/**/*.js', ['jshint']);
     gulp.watch('source/styles/*.less', ['less']);
 });
 
-//gulp tasks
+// tasks
 gulp.task('less', function() {
+    log('Compiling LESS');
     return gulp.src('source/styles/index.less')
         .pipe(plumber({
             errorHandler: function(err) {
@@ -26,6 +30,7 @@ gulp.task('less', function() {
 });
 
 gulp.task('jshint', function() {
+    log('Checking JS');
     return gulp.src('source/app/**/*.js')
         .pipe(plumber({
             errorHandler: function(err) {
@@ -38,5 +43,30 @@ gulp.task('jshint', function() {
 });
 
 
-//gulp commands
-gulp.task('default', ['watch']);
+gulp.task('inject', function() {
+    var
+        wiredep = require('wiredep').stream,
+        jsSources = ['source/app/app.js', 'source/app/**/*.module.js', 'source/app/**/*.js'],
+        cssSources = ['source/styles/*.css'],
+        sources = [].concat(jsSources, cssSources);
+
+    log('Injecting files');
+
+    return gulp.src('source/app/index.tpl.html')
+        .pipe(wiredep({ ignorePath: '../' }))
+        .pipe(inject(gulp.src(sources), { ignorePath: '/source' }))
+        .pipe(rename('index.html'))
+        .pipe(gulp.dest('source'));
+});
+
+// commands
+gulp.task('default', ['inject']);
+
+//TODO add dev build
+//TODO add prod build
+
+//=============Functions==============
+
+function log(msg) {
+    util.log(util.colors.blue(msg));
+}
