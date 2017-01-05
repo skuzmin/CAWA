706fac4d5fb537876fbfc88b1ebdebac99f14f70
skuzmin <erngorn@gmail.com>
2017-01-05 20:46:57 +0200

    added slug ( basic)

--- source/app/decision/decision.route.js
+++ source/app/decision/decision.route.js
@@ -11,20 +11,36 @@
     function configuration($stateProvider) {
         $stateProvider
             .state('decision', {
-                url: '/decisions/:id',
+                url: '/decision/:id/{slug}/{criteria}',
                 templateUrl: 'app/decision/decision.html',
                 controller: 'DecisionController',
                 controllerAs: 'vm',
                 resolve: {
                     decisionBasicInfo: DecisionResolver
+                },
+                params: {
+                    slug: { value: null, squash: true },
+                    criteria: { value: null, squash: true }
                 }
             });
     }
 
-    DecisionResolver.$inject = ['DecisionService', '$stateParams'];
+    DecisionResolver.$inject = ['DecisionService', '$stateParams', '$state', '$rootScope'];
 
-    function DecisionResolver(DecisionService, $stateParams) {
-        return DecisionService.getDecisionInfo($stateParams.id);
+    function DecisionResolver(DecisionService, $stateParams, $state, $rootScope) {
+        return DecisionService.getDecisionInfo($stateParams.id).then(function(result) {
+            // var stateListener = $rootScope.$on('$stateChangeSuccess',
+            //     function(event, toState, toParams, fromState, fromParams) {
+            //         if (toParams.slug !== result.nameSlug) {
+            //             $stateParams.slug = result.nameSlug;
+            //             $state.go('decision', $stateParams, {notify: false});
+            //         }
+            //         stateListener();
+            //     });
+            return result;
+        }).catch(function() {
+            $state.go('404');
+        });
     }
 
 })();
