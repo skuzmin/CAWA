225f8ded0bd30026bc6b2217e48a54b45308d0ce
Marko Zub <mzub@n-ix.com>
2017-02-27 17:42:29 +0200

    fix matrix aside on scroll

--- source/app/desicionMatrix/decision-matrix.controller.js
+++ source/app/desicionMatrix/decision-matrix.controller.js
@@ -336,7 +336,7 @@
 
             criteriaGroupsCount = vm.criteriaGroups[0].criteria.length || 0;
             characteristicGroupsCount = vm.characteristicGroups[0].characteristics.length || 0;
-            vm.tableWidth = (criteriaGroupsCount + characteristicGroupsCount) * 120 + 'px';
+            vm.tableWidth = (criteriaGroupsCount + characteristicGroupsCount) * 120 + 60 + 'px';
         }
 
         // TODO: make as separeted component
