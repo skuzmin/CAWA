ed34caeeb45e336c71d1b72d47fd111c4be959ba
Marko Zub <mzub@n-ix.com>
2017-03-20 20:54:41 +0200

    update discussions

--- source/app/discussions/discussion-decision-child.controller.js
+++ source/app/discussions/discussion-decision-child.controller.js
@@ -6,9 +6,9 @@
         .module('app.discussions')
         .controller('DiscussionDecisionChildController', DiscussionDecisionChildController);
 
-    DiscussionDecisionChildController.$inject = ['decisionBasicInfo', 'DiscussionsDataService', '$stateParams', '$rootScope', 'DecisionDataService'];
+    DiscussionDecisionChildController.$inject = ['decisionBasicInfo', 'DiscussionsDataService', '$stateParams', '$rootScope', 'DecisionDataService', '$q'];
 
-    function DiscussionDecisionChildController(decisionBasicInfo, DiscussionsDataService, $stateParams, $rootScope, DecisionDataService) {
+    function DiscussionDecisionChildController(decisionBasicInfo, DiscussionsDataService, $stateParams, $rootScope, DecisionDataService, $q) {
         var vm = this;
         vm.decision = decisionBasicInfo || {}; //Parent Decision
 
@@ -36,10 +36,67 @@
             });
         }
 
+        function fillRating(decisionMatrixList) {
+            // vm.criteriaGroups = 
+            _.map(vm.criteriaGroups, function(group) {
+                _.map(group.criteria, function(criteria) {
+
+                    var criteriaNew = _.clone(criteria);
+                    _.map(decisionMatrixList.criteria, function(criteriaMatrix) {
+                        // console.log(criteriaMatrix.criterionId, criteriaNew.criterionId, criteriaMatrix.criterionId === criteriaNew.criterionId);
+                        if (criteriaMatrix.criterionId === criteriaNew.criterionId) {
+                            criteria.totalDislikes = criteriaMatrix.totalDislikes;
+                            criteria.totalLikes = criteriaMatrix.totalLikes;
+                            criteria.totalVotes = criteriaMatrix.totalVotes;
+                            criteria.weight = criteriaMatrix.weight;
+                        }
+                    });
+                });
+            });
+
+
+            // // console.log(decisionMatrixList);
+            // _.map(vm.characteristicGroups, function(group) {
+            //     // console.log(group);
+            //     _.map(group.characteristics, function(characteristic) {
+            //         // console.log(characteristic);
+
+            //         var characteristicNew = _.clone(characteristic);
+            //         _.map(decisionMatrixList.characteristics, function(characteristicMatrix) {
+            //             // console.log(characteristicMatrix.criterionId, characteristicNew.criterionId, characteristicMatrix.criterionId === characteristicNew.criterionId);
+            //             if (characteristicMatrix.characteristicId === characteristicNew.characteristicId) {
+            //                 characteristic.totalDislikes = characteristicMatrix.totalDislikes;
+            //                 characteristic.totalLikes = characteristicMatrix.totalLikes;
+            //                 characteristic.totalVotes = characteristicMatrix.totalVotes;
+            //                 characteristic.weight = characteristicMatrix.weight;
+
+            //                 console.log(characteristic);
+            //             }
+            //         });
+            //     });
+            // });
+        }
+
         function init() {
             console.log('Discussion Decision controller');
-            getCriteriaGroupsById(vm.decision.decisionId);
-            getCharacteristictsGroupsById(vm.decision.decisionId);
+
+            // Get criteria and characteristic
+            $q.all([getCriteriaGroupsById(vm.decision.decisionId),
+                    getCharacteristictsGroupsById(vm.decision.decisionId)
+                ])
+                .then(function(values) {
+
+                    // Get Rating
+                    var sendData = {
+                        includeChildDecisionIds: []
+                    };
+
+                    sendData.includeChildDecisionIds.push($stateParams.discussionId);
+                    DecisionDataService.searchDecisionMatrix(vm.decision.decisionId, sendData).then(function(result) {
+
+                        fillRating(result.decisionMatrixs[0]);
+                    });
+                });
 
             // Child Decision
             DecisionDataService.getDecisionInfo($stateParams.discussionId).then(function(result) {
@@ -59,6 +116,7 @@
                     link: null
                 }];
             });
+
 
         }
     }
