9b10b9d85ed699445c99bdbab422a27110fe71e3
Marko Zub <mzub@n-ix.com>
2017-03-20 14:51:29 +0200

    merge fixs

--- source/app/components/ratingStar/rating-star.component.js
+++ source/app/components/ratingStar/rating-star.component.js
@@ -25,19 +25,23 @@
         vm.$onChanges = onChanges;
         vm.showRating = true;
 
-        
+
         function onChanges() {
             if (vm.value) value = vm.value.toString();
             vm.rating = value;
             if(!vm.totalVotes) vm.totalVotes = 0;
             // calc default rating widthout %
-            if (value && value.indexOf('%') === -1) {          
-
-            function onChanges() {
+            if (value && value.indexOf('%') === -1) {
                 vm.rating = parseFloat(vm.value) / AppRatingStarConstant.MAX_RATING * 100 + '%' || 0;
                 vm.value = vm.value || 0;
                 vm.showRating = parseInt(vm.value) > 0;
             }
+        }
+
+        init();
+
+        function init() {
+
         }
     }
 })();
